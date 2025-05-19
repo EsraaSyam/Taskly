608090be699b6f6b511dfde5b9e667ab101fcf7b
EsraaSyam <e294014@gmail.com>
2025-05-19 12:56:53 +0300

    feat(task): add get operations

--- src/main/java/com/example/taskly/task/TaskController.java
+++ src/main/java/com/example/taskly/task/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/task")
@@ -27,4 +28,17 @@ public class TaskController {
                 .created(URI.create("/task/" + response.getId()))
                 .body(response);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskResponseDTO> getTask(@PathVariable long id) {
+        TaskResponseDTO response = taskService.getTask(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<TaskResponseDTO>> getAllTasks() {
+        List<TaskResponseDTO> response = taskService.getAllTasks();
+        return ResponseEntity.ok(response);
+    }
+
 }
